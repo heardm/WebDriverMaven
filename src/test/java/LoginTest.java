d810fcaeee9079faafd78823be6cfd1d5c518a98
Jena <C5249422@global.corp.sap>
2016-12-11 09:46:11 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp()
+	{
+		driver= new FirefoxDriver();
+	}
+	@Test
+	public void doLogin() throws InterruptedException
+	{
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("aaa@gmail.com");
+		Thread.sleep(2000L);
+		
+	}
+	@AfterTest
+	public void TeatDown()
+	{
+		driver.quit();
+	}
+
+}
